335ea7ea35d990fbd9e27fe526bcb7c3d8c2d9d4
Steffen Haefelinger functionfactory.de <mail@functionfactory.de>
2021-08-28 14:46:12 +0200

    test commit from sourcetree

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -30,7 +30,7 @@ void PFMProject0AudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("Hello World! aha", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void PFMProject0AudioProcessorEditor::resized()
